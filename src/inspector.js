d03997ee67fef148d97803a9fb53819377a8deaa
Rolando Murillo <rolando@bandd.co>
2014-03-19 01:05:36 -0400

    Fancy animation + instructions.

--- src/inspector.js
+++ src/inspector.js
@@ -170,6 +170,7 @@
         _.hide(_.id('mixResult'));
         _.hide(_.id('mixControls'));
         _.hide(_.id('tipSecondColor'))
+        if (!settings.tip.tipSecondColor) { settings.tip.tipSecondColor = false }
         return _.listen('keydown', function(event) {
           if (event.altKey) {
             return event.preventDefault();
